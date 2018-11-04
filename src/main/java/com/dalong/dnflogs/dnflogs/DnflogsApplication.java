a501963bd58814e1ce97164d1f9152b8e260875b
dalong <dalong@coupang.com>
2018-11-04 19:17:13 +0900

    first commit

---
+++ src/main/java/com/dalong/dnflogs/dnflogs/DnflogsApplication.java
@@ -0,0 +1,12 @@
+package com.dalong.dnflogs.dnflogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnflogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnflogsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dalong/dnflogs/dnflogs/DnflogsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dalong.dnflogs.dnflogs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DnflogsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
